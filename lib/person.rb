38267b90b0fa2ff2351fb178cc02bc1fb77f9629
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 23:08:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,7 +7,6 @@ def initialize(name:, birthday:, hair_color:, eye_color:, height:,
 weight:, handed:, complexion:, t_shirt_size:, 
 wrist_size:, glove_size:, pant_length:, pant_width:)
 
-attributes.each {|key, value| self.send(("#{key}="), value)}
 @name = name
 @birthday = birthday 
 @hair_color = hair_color
