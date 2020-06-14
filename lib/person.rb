98b2e1d040af0a3e1b000b8303cbb9d9de11045c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 23:16:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(t)
+def initialize(person_attributes)
 
 @name = name
 @birthday = birthday 
