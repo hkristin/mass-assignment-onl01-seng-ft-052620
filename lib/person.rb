ae8257002d290e07a290a0a0aefe3beeb70d71fd
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 23:15:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,9 +3,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize(name:, birthday:, hair_color:, eye_color:, height:, 
-weight:, handed:, complexion:, t_shirt_size:, 
-wrist_size:, glove_size:, pant_length:, pant_width:)
+def initialize
 
 @name = name
 @birthday = birthday 
@@ -22,7 +20,7 @@ wrist_size:, glove_size:, pant_length:, pant_width:)
 @pant_width = pant_width
 end
 
-instance_of_person.key = value
+
 
 end
 
